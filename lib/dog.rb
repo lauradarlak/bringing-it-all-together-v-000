ac4dc7eee945020cac1999a44c0354685587fe55
Laura Darlak <laura@polyporedesign.com>
2018-10-09 17:38:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -93,4 +93,9 @@ class Dog
     self.new(id: row[0], name: row[1], breed: row[2])
   end
 
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
 end
