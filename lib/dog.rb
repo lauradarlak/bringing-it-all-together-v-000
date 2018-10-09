d8e00bb7d5ac65bce7178d429f6ac2c0a0cd95cd
Laura Darlak <laura@polyporedesign.com>
2018-10-09 17:28:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,10 +65,11 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed) # => [[1, "Hello", "25"]]
     if !dog.empty?
       dog_data = dog[0] # => [[1, "Hello", "25"]]
-      self.new(dog_data)
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else # create and save a new Dog instance
-      self.create(name: name, breed: breed)
+      dog = self.create(name: name, breed: breed)
     end
+    dog # return dog object, whether found or created
   end
 
 
