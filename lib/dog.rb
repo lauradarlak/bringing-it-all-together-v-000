3e2f9352fca4c621679de554ed57749c99cccb56
Laura Darlak <laura@polyporedesign.com>
2018-10-09 17:26:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,9 +67,8 @@ class Dog
       dog_data = dog[0] # => [[1, "Hello", "25"]]
       self.new(dog_data)
     else # create and save a new Dog instance
-      dog = self.create(name: name, breed: breed)
+      self.create(name: name, breed: breed)
     end
-    dog # return dog object, whether found or created
   end
 
 
