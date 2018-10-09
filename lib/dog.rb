9a599cc4c0ac675526e7643eab969293ff17dec8
Laura Darlak <laura@polyporedesign.com>
2018-10-09 16:45:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(dog_hash)
+  def initialize(dog_hash, id=nil)
     dog_hash.each{|k,v| self.send(("#{k}="), v)}
 
   end
