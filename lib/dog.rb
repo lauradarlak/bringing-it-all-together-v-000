09d8dc12be557bd3aef99072012609c9f994e64b
Laura Darlak <laura@polyporedesign.com>
2018-10-09 17:32:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,9 +77,8 @@ class Dog
     dog
   end
 
-
-  # def self.new_from_db(row)
-  #   self.new(id: row[0], name: row[1], breed: row[2])
-  # end
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
 
 end
