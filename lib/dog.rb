d7612f1a5ee8791c89829136d61d3c27a1e88890
Laura Darlak <laura@polyporedesign.com>
2018-10-09 17:37:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,7 @@ class Dog
     Dog.new(id: row[0], name: row[1], breed: row[2])
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM dogs
       WHERE name = ?
